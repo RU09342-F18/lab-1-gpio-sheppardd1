0758f6d4dc1bd5717a357f404773b63cc6a86bc8
David <sheppardd1@students.rowan.edu>
2018-09-15 12:03:46 -0400

    built basic program

--- Button Blink/MSP430G2553/main.c
+++ Button Blink/MSP430G2553/main.c
@@ -4,9 +4,19 @@
 /**
  * main.c
  */
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+int main(void){
+    P1SEL &= !BIT0;
+    P1SEL2 &= !BIT0;
+    P1DIR |= BIT0;
+    while(1){
+        P1OUT ^= BIT0;
+        int i = 0;
+        int j = 0;
+        while(i < 10000000){
+            i++;
+            while(j < 10000000){
+              j++;
+            };
+        };
+    }
 }
