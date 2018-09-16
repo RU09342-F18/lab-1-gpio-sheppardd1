454485e4c9c7a15d6ebcfdc44da88bed0561107b
David <sheppardd1@students.rowan.edu>
2018-09-16 16:33:06 -0400

    minor edits

--- Button Blink/MSP430G2553/main.c
+++ Button Blink/MSP430G2553/main.c
@@ -70,28 +70,29 @@
 //David Sheppard
 //16 September 2018
 //Lab 1: Button Blink for MSP430G2553
+//Allows user to control LEDs using the pin 3 button
 
 #include <msp430x20x2.h>
 
-#define LED0 BIT0
-#define LED1 BIT6
-#define BUTTON BIT3
+#define LED0 BIT0   //defining LED0 as BIT0
+#define LED1 BIT6   //defining LED1 as BIT6
+#define BUTTON BIT3 //defining BUTTON as BIT3
 
 
 
-int main(void)
+int main(void)  //begin main function
 {
-WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
-P1DIR |= (LED0 + LED1); // Set P1.0 to output direction
-// P1.3 must stay at input
-P1OUT &= ~(LED0 + LED1); // set P1.0 to 0 (LED OFF)
-P1IE |= BUTTON; // P1.3 interrupt enabled
+    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+    P1DIR |= (LED0 + LED1); // Set P1.0 to output direction
+    // P1.3 must stay at input
+    P1OUT &= ~(LED0 + LED1); // set P1.0 to 0 (LED OFF)
+    P1IE |= BUTTON; // P1.3 interrupt enabled
 
-P1IFG &= ~BUTTON; // P1.3 IFG cleared
+    P1IFG &= ~BUTTON; // P1.3 IFG cleared
 
-__enable_interrupt(); // enable all interrupts
-for(;;)
-{}
+    __enable_interrupt(); // enable all interrupts
+    for(;;)
+    {}
 }
 
 
