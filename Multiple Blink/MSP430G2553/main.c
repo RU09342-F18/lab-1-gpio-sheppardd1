75d97995986276e73a80b35b269dc51fd9a005f1
David <sheppardd1@students.rowan.edu>
2018-09-16 15:23:47 -0400

    minor updates

--- Multiple Blink/MSP430G2553/main.c
+++ Multiple Blink/MSP430G2553/main.c
@@ -13,11 +13,13 @@ int main(void)  //begin main function
     P1OUT = 0;                      // All P1.x reset to 0
     P2DIR = 0xFF;                   // All P2.x outputs set to 1
     P2OUT = 0;                      // All P2.x reset to 0
+
+    //P1.0 has a delay of 1/2 second and P1.6 has a delay of 1/4 secondf
     while(1){
         P1OUT ^= 0x40;              // Invert value of P1OUT Pin 6 (red LED)
-        __delay_cycles(250000);     // Set delay to 1/2 second (LED is on for 1/2 second and then off for 1/2 second)
+        __delay_cycles(250000);     // Set delay to 1/4 second
         P1OUT ^= 0x41;              //Invert P1OUT Pins 6 and 0
-        __delay_cycles(250000);
+        __delay_cycles(250000);     //add another 1/4 second delay
     }
 
     return 0;
